167c2d03532730a495b9ce8023cb5a5b10165461
Nahid Hasan <nahidhasan282@gmail.com>
2021-11-27 11:00:32 +0600

    added topheadline model

--- apiconsumer/models.py
+++ apiconsumer/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
-class NewsModel(models.Model):
-    pass
+class SourceModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    sourceId = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.CharField(max_length=255)
+    country = models.CharField(max_length=100)
+
+class TopHeadline(models.Model):
+    id = models.AutoField(primary_key=True)
+    headline = models.CharField(max_length=100)
+    source = models.ManyToManyField(SourceModel)
+    country = models.CharField(max_length=100)
