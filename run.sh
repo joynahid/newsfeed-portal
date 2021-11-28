9186b7cd61f89d3d7ca643ce25f163c8915845a0
Nahid Hasan <nahidhasan282@gmail.com>
2021-11-28 20:16:57 +0600

    added some startup scripts for convenience

---
+++ run.sh
@@ -0,0 +1,4 @@
+docker-compose up -d --build djangoapp
+sleep 5s
+docker exec djangoapp /scripts/make.sh
+docker-compose down
---
+++ scripts/startup.py
@@ -0,0 +1,7 @@
+from apiconsumer.tasks import feed_sources_sync, feed_headlines_sync
+from dotenv import load_dotenv
+
+load_dotenv('/code/.env')
+
+feed_sources_sync()
+feed_headlines_sync()
