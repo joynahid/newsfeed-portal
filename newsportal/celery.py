75f7e6f91acb003c39583ff504ca9f43bc3f7466
Nahid Hasan <nahidhasan282@gmail.com>
2021-11-29 13:44:59 +0600

    tweaked headline consuming time

--- newsportal/celery.py
+++ newsportal/celery.py
@@ -7,17 +7,17 @@ app = Celery("newsportal")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.conf.beat_schedule = {
-    "feed-news-every-15-minutes": {
+    "feed-news-every-12-minutes": {
         "task": "apiconsumer.tasks.feed_headlines",
-        "schedule": 60*15,
+        "schedule": 60*12,
     },
     "sync-sources-every-24-hours": {
         "task": "apiconsumer.tasks.feed_sources",
         "schedule": 24*60*60,
     },
-    "sync-newsfeed-every-15-minutes": {
+    "sync-newsfeed-every-14-minutes": {
         "task": "newsfeed.tasks.populate_newsfeed",
-        "schedule": 15*60,
+        "schedule": 14*60,
     },
     "notify-user-every-30-minutes": {
         "task": "newsfeed.tasks.notify_headlines",
